38ea37765bfdc14c5737935e26dc8f89a861ed6c
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-16 19:08:58 -0700

    Route fix

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251017_020408 from './20251017_020408';
-
-export const migrations = [
-  {
-    up: migration_20251017_020408.up,
-    down: migration_20251017_020408.down,
-    name: '20251017_020408'
-  },
-];
