58b80dfd1fe5b28d0f6c92cfe7551a316d83d018
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-26 02:21:54 -0700

    Fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251026_090546 from './20251026_090546';
+import * as migration_20251026_090546 from './20251026_090546'
 
 export const migrations = [
   {
     up: migration_20251026_090546.up,
     down: migration_20251026_090546.down,
-    name: '20251026_090546'
+    name: '20251026_090546',
   },
-];
+]
