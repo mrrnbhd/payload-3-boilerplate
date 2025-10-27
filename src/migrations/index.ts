770f8f7eafb0562245b4f72b199fc765e12a44ab
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-26 17:44:18 -0700

    Delete old migration

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251026_233842_uploads_to_files from './20251026_233842_uploads_to_files';
-
-export const migrations = [
-  {
-    up: migration_20251026_233842_uploads_to_files.up,
-    down: migration_20251026_233842_uploads_to_files.down,
-    name: '20251026_233842_uploads_to_files'
-  },
-];
