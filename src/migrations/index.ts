74e78f8473dca97aacce85d7e6fb81714fbef3c9
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-27 17:08:55 -0700

    Migration

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251027_230939 from './20251027_230939';
-
-export const migrations = [
-  {
-    up: migration_20251027_230939.up,
-    down: migration_20251027_230939.down,
-    name: '20251027_230939'
-  },
-];
