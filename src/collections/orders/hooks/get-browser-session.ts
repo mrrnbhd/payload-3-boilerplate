950a0d9cb742a6ee805c990a1ec2391fe25d7d8e
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-27 17:26:03 -0700

    Steel

--- src/collections/orders/hooks/get-browser-session.ts
+++ src/collections/orders/hooks/get-browser-session.ts
@@ -5,7 +5,7 @@ const STEEL_API_KEY = process.env.STEEL_API_KEY ?? 'No Steel Browser API key fou
 
 const client: Steel = new Steel({
   steelAPIKey: STEEL_API_KEY,
-  baseURL: `http://${process.env.STEEL_PRIVATE_URL}`,
+  baseURL: `https://${process.env.STEEL_URL}`,
 })
 //
 export const getBrowserSession: CollectionBeforeChangeHook = async ({ data, operation }) => {
