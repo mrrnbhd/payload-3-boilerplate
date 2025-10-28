56289496df3d0fae6ef3398a36fc706905bf4b0d
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-27 19:41:56 -0700

    Browser conditions

--- src/collections/orders/hooks/get-browser-session.ts
+++ src/collections/orders/hooks/get-browser-session.ts
@@ -8,34 +8,31 @@ const client: Steel = new Steel({
   baseURL: `https://${process.env.STEEL_URL}`,
 })
 //
-export const getBrowserSession: CollectionBeforeChangeHook = async ({ data, operation }) => {
+export const getBrowserSession: CollectionBeforeChangeHook = async ({ data }) => {
   if (
-    operation === 'create' &&
     !data.sessionURL &&
-    data.status !== 'Fulfilled' &&
+    !['Purchased', 'Fulfilled', 'Failure'].includes(data.status) &&
     data.orderNumber
   ) {
     try {
-      if (data.status !== 'Purchased') {
-        console.log('\nCreating Steel session...')
+      console.log('\nCreating Steel session...')
 
-        const session = await client.sessions.create({
-          sessionId: data.orderNumber,
-          solveCaptcha: true,
-          stealthConfig: {
-            humanizeInteractions: true,
-          },
-          deviceConfig: {
-            device: 'mobile',
-          },
-        })
+      const session = await client.sessions.create({
+        sessionId: data.orderNumber,
+        solveCaptcha: true,
+        stealthConfig: {
+          humanizeInteractions: true,
+        },
+        deviceConfig: {
+          device: 'mobile',
+        },
+      })
 
-        console.log(
-          `\x1b[1;93mSteel Session created!\x1b[0m\n` +
-            `View session at \x1b[1;37m${session.sessionViewerUrl}\x1b[0m`
-        )
-        data.sessionURL = session.sessionViewerUrl
-      }
+      console.log(
+        `\x1b[1;93mSteel Session created!\x1b[0m\n` +
+          `View session at \x1b[1;37m${session.sessionViewerUrl}\x1b[0m`
+      )
+      data.sessionURL = session.sessionViewerUrl
     } catch (error) {
       console.error('An error occurred creating Steel session:', error)
     }
