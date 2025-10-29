8cbc1de7efa0843bbbcc3dacc5a991985e7b83d7
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-28 20:36:55 -0700

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251029_005831 from './20251029_005831';
+import * as migration_20251029_033653 from './20251029_033653';
 
 export const migrations = [
   {
     up: migration_20251029_005831.up,
     down: migration_20251029_005831.down,
-    name: '20251029_005831'
+    name: '20251029_005831',
+  },
+  {
+    up: migration_20251029_033653.up,
+    down: migration_20251029_033653.down,
+    name: '20251029_033653'
   },
 ];
