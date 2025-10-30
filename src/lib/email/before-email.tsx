ebfa63f72998615ea20b068cae4745cbd7c9fae1
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-29 20:48:28 -0700

    Clean-Up

--- src/lib/email/before-email.tsx
+++ src/lib/email/before-email.tsx
@@ -1,7 +1,7 @@
 import type { BeforeEmail } from '@payloadcms/plugin-form-builder/types'
 import { render } from '@react-email/render'
 import parse from 'html-react-parser'
-import TicketerTemplate from './email-template'
+import { TicketerTemplate } from './email-template'
 
 const beforeEmail: BeforeEmail = (emailsToSend, _beforeChangeParams) => {
   return Promise.all(
