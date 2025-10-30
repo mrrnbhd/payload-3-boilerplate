e13a46e11cdedaf5a70345d881f89443b2b25ba1
Mr. Rogers <129814015+mr-rogers-neighborhood@users.noreply.github.com>
2025-10-29 20:52:08 -0700

    Env

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,8 @@ import z from 'zod'
 
 export const envSchema = z.object({
   DATABASE_URI: z.string(),
+  STEEL_API_KEY: z.string(),
+  STEEL_URL: z.string(),
   PAYLOAD_SECRET: z.string(),
   NEXT_PUBLIC_SERVER_URL: z.string(),
   CRON_SECRET: z.string(),
